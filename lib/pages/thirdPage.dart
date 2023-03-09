6c0bd63a66888c4da3f7c4c70c597143aa38dc89
lokugama-vyn <62249471+lokugama-vyn@users.noreply.github.com>
2023-03-09 14:35:59 +0530

    added topics to receive topics from ros node

--- lib/pages/thirdPage.dart
+++ lib/pages/thirdPage.dart
@@ -26,7 +26,7 @@ class _thirdPageState extends State<thirdPage> {
   late Topic damage_detects;
   var selectedTab = 1;
   String new1 = '';
-  bool damage = false;
+  bool damage = true;
   @override
   void initState() {
     // TODO: implement initState
@@ -34,7 +34,7 @@ class _thirdPageState extends State<thirdPage> {
     battery_state = Topic(
       ros: ros,
       name: '/topic',
-      type: "std_msgs/Float64",
+      type: "std_msgs/String",
       reconnectOnClose: true,
       queueSize: 10,
       queueLength: 10,
@@ -42,7 +42,7 @@ class _thirdPageState extends State<thirdPage> {
     hours_cleaned = Topic(
       ros: ros,
       name: '/topic',
-      type: "std_msgs/Float64",
+      type: "std_msgs/String",
       reconnectOnClose: true,
       queueSize: 10,
       queueLength: 10,
@@ -50,14 +50,14 @@ class _thirdPageState extends State<thirdPage> {
     damage_detects = Topic(
       ros: ros,
       name: '/topic',
-      type: "std_msgs/Float64",
+      type: "std_msgs/String",
       reconnectOnClose: true,
       queueSize: 10,
       queueLength: 10,
     );
     super.initState();
     mymethod();
-    initConnection();
+    //initConnection();
   }
 
   void initConnection() async {
@@ -88,13 +88,18 @@ class _thirdPageState extends State<thirdPage> {
     battery_status = json.encode(msg);
     setState(() {});
   }
-  //TOPIC HANDLING
 
-  //need to add code
-  void battery_state_func() async {}
-  void hours_cleaned_func() async {}
-  void damage_detects_func() async {}
-  //afterwards didnt add code for topics
+  //TOPIC HANDLING
+  void destroyConnection() async {
+    //await chatter.unsubscribe(); //forward unadvertise?
+    await damage_detects.unsubscribe();
+    await hours_cleaned.unsubscribe();
+    await battery_state.unsubscribe();
+
+    await ros.close();
+    setState(() {});
+  }
+
   late int? vertical;
   late int? horizontal;
   Future<void> mymethod() async {
@@ -127,156 +132,184 @@ class _thirdPageState extends State<thirdPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Monitoring Panel"),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.deepOrange,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.purple,
-                    ),
-                    onPressed: _cleaning,
-                    child: Text(
-                      "Cleaning",
-                      style: TextStyle(
-                          fontWeight: selectedTab == 1
-                              ? FontWeight.bold
-                              : FontWeight.normal),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 20),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(206, 148, 211, 46),
-                    ),
-                    onPressed: _cleaning_finished,
-                    child: Text(
-                      "Cleaning Finished",
-                      style: TextStyle(
-                          fontWeight: selectedTab == 2
-                              ? FontWeight.bold
-                              : FontWeight.normal),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height - 130,
-              child: LayoutBuilder(builder: (context, constraints) {
-                if (selectedTab == 1) {
-                  return Container(
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                          Color.fromARGB(255, 203, 43, 147),
-                          Color.fromARGB(255, 149, 70, 196),
-                          Color.fromARGB(255, 94, 97, 244),
-                        ])),
-                    child: Scaffold(
-                      backgroundColor: Colors.transparent,
-                      body: Center(
-                        child: Column(children: <Widget>[
-                          Image.asset(
-                            "assets/images/ani.gif",
-                            height: 300,
-                            width: 300,
-                          ),
-                        ]),
-                      ),
-                    ),
-                  );
-                } else {
-                  return Container(
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                          Color.fromARGB(255, 203, 43, 147),
-                          Color.fromARGB(255, 149, 70, 196),
-                          Color.fromARGB(255, 94, 97, 244),
-                        ])),
-                    child: Scaffold(
-                      backgroundColor: Colors.transparent,
-                      body: Padding(
-                        padding: const EdgeInsets.only(left: 15, right: 15),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.only(right: 15),
-                            ),
-                            Expanded(
-                              // Added
-                              child: Container(
-                                  child: Column(
-                                children: [
-                                  Expanded(
-                                    child: GridView.count(
-                                      crossAxisCount: 3,
-                                      crossAxisSpacing: 4.0,
-                                      mainAxisSpacing: 8.0,
-                                      children: List.generate(choices.length,
-                                          (index) {
-                                        return Center(
-                                          child: SelectCard(
-                                              choice: choices[index]),
-                                        );
-                                      }),
-                                    ), // Added
-                                  ),
-                                ],
-                              )),
-                            ),
-                            Container(
-                                height:
-                                    MediaQuery.of(context).size.height - 280,
-                                width: double.infinity,
-                                child: damage
-                                    ? Container(
-                                        decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                            image: AssetImage(
-                                                'assets/images/robot.jpg'),
-                                            fit: BoxFit.fill,
-                                          ),
-                                        ),
-                                      )
-                                    : Container(
-                                        decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                            image: AssetImage(
-                                                'assets/images/caution.jpg'),
-                                            fit: BoxFit.fill,
-                                          ),
-                                        ),
-                                      )),
-                          ],
+    return StreamBuilder<Object>(
+      stream: ros.statusStream,
+      builder: (context, snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Monitoring Panel"),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+          ),
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  color: Colors.deepOrange,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.purple,
+                        ),
+                        onPressed: _cleaning,
+                        child: Text(
+                          "Cleaning",
+                          style: TextStyle(
+                              fontWeight: selectedTab == 1
+                                  ? FontWeight.bold
+                                  : FontWeight.normal),
                         ),
                       ),
-                    ),
-                  );
-                }
-              }),
-            )
-          ],
-        ),
-      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 20),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(206, 148, 211, 46),
+                        ),
+                        onPressed: _cleaning_finished,
+                        child: Text(
+                          "Cleaning Finished",
+                          style: TextStyle(
+                              fontWeight: selectedTab == 2
+                                  ? FontWeight.bold
+                                  : FontWeight.normal),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height - 130,
+                  child: LayoutBuilder(builder: (context, constraints) {
+                    if (selectedTab == 1) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                              Color.fromARGB(255, 203, 43, 147),
+                              Color.fromARGB(255, 149, 70, 196),
+                              Color.fromARGB(255, 94, 97, 244),
+                            ])),
+                        child: Scaffold(
+                          backgroundColor: Colors.transparent,
+                          body: Center(
+                            child: Column(children: <Widget>[
+                              Image.asset(
+                                "assets/images/ani.gif",
+                                height: 300,
+                                width: 300,
+                              ),
+                            ]),
+                          ),
+                        ),
+                      );
+                    } else {
+                      return Container(
+                        decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                              Color.fromARGB(255, 203, 43, 147),
+                              Color.fromARGB(255, 149, 70, 196),
+                              Color.fromARGB(255, 94, 97, 244),
+                            ])),
+                        child: Scaffold(
+                          backgroundColor: Colors.transparent,
+                          body: Padding(
+                            padding: const EdgeInsets.only(left: 15, right: 15),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.only(right: 15),
+                                ),
+                                Expanded(
+                                  // Added
+                                  child: Container(
+                                      child: Column(
+                                    children: [
+                                      ActionChip(
+                                        label: Text(
+                                            snapshot.data == Status.connected
+                                                ? 'DISCONNECT'
+                                                : 'CONNECT'),
+                                        backgroundColor:
+                                            snapshot.data == Status.connected
+                                                ? Colors.green[300]
+                                                : Colors.grey[300],
+                                        onPressed: () {
+                                          //print(snapshot.data);
+                                          if (snapshot.data !=
+                                              Status.connected) {
+                                            this.initConnection();
+                                          } else {
+                                            this.destroyConnection();
+                                          }
+                                        },
+                                      ),
+                                      Expanded(
+                                        child: GridView.count(
+                                          crossAxisCount: 3,
+                                          crossAxisSpacing: 4.0,
+                                          mainAxisSpacing: 8.0,
+                                          children: List.generate(
+                                              choices.length, (index) {
+                                            return Center(
+                                              child: SelectCard(
+                                                  choice: choices[index]),
+                                            );
+                                          }),
+                                        ),
+
+                                        // Added
+                                      ),
+                                    ],
+                                  )),
+                                ),
+                                Container(
+                                    height: MediaQuery.of(context).size.height -
+                                        280,
+                                    width: double.infinity,
+                                    padding:
+                                        EdgeInsets.only(top: 15, bottom: 10),
+                                    child: damage
+                                        ? Container(
+                                            decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                image: AssetImage(
+                                                    'assets/images/caution.jpg'),
+                                                fit: BoxFit.fill,
+                                              ),
+                                            ),
+                                          )
+                                        : Container(
+                                            decoration: BoxDecoration(
+                                              image: DecorationImage(
+                                                image: AssetImage(
+                                                    'assets/images/robot.jpg'),
+                                                fit: BoxFit.fill,
+                                              ),
+                                            ),
+                                          )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  }),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -288,9 +321,9 @@ class Choice {
 }
 
 const List<Choice> choices = const <Choice>[
-  const Choice(title: 'Hours Taken : ', icon: Icons.access_alarm),
-  const Choice(title: 'Battery Level : ', icon: Icons.battery_charging_full),
-  const Choice(title: 'Damage Detection : ', icon: Icons.cameraswitch_rounded),
+  const Choice(title: 'Hours Taken :', icon: Icons.access_alarm),
+  const Choice(title: 'Battery Level :', icon: Icons.battery_charging_full),
+  const Choice(title: 'Damage Detection :', icon: Icons.cameraswitch_rounded),
 ];
 
 class SelectCard extends StatelessWidget {
