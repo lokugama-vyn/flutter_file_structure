1cf9b74678a7a10c09351468443400fdc19e3b27
lokugama-vyn <62249471+lokugama-vyn@users.noreply.github.com>
2023-04-05 22:18:23 +0530

    added debugshowcheckedmodebanner cmd

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const SignInScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
